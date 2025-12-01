7ed2940194d31b621e2d3a6164352cbf043f7627
Jon Rey L. Lumayag - Office <jonreylumayag@gmail.com>
2025-12-02 07:46:34 +0800

    Add forwarded host

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "edtrm.region9.dilg.gov.ph",
+        "127.0.0.1:3000",
+        "172.20.82.83",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
